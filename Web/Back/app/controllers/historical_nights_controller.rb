a3acaa620677b6db1f560b9aa24eb4b146b68964
Javier Pérez <javier.perez.ca@usach.cl>
2021-03-07 23:48:40 -0300

    FEAT: Add Last_Night Controller and Historical_Nights Controller.

---
+++ Web/Back/app/controllers/historical_nights_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Controller that connects to:
+# - Cassandra to get the historical nights infomarmation.
+class HistoricalNightsController < ApplicationController
+  def historical_night; end
+
+  def historical_night_payload; end
+end
---
+++ Web/Back/app/controllers/last_night_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Controller that connects to:
+# - Redis to get the last night information.
+class LastNightController < ApplicationController
+  def last_night; end
+
+  def last_night_payload; end
+end
