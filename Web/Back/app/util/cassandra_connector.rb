03a022421a28b7bd892e63d3a6c8f8e78f1419f9
Javier Pérez <javier.perez.ca@usach.cl>
2021-03-09 22:46:50 -0300

    Feat: Added Cequel.

--- Web/Back/app/util/cassandra_connector.rb
+++ Web/Back/app/util/cassandra_connector.rb
@@ -6,7 +6,7 @@ require 'cassandra'
 class CassandraConnector
   @cassandra = nil
   def self.get
-    @cassandra = Cassandra.new('datacenter1', '127.0.0.1:9042')
+    @cassandra = Cassandra.new('astro_keyspace', '127.0.0.1:9042')
     @cassandra
   end
 end
