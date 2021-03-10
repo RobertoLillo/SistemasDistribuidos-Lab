b7ed396860a162427d6276f5f1039442b203ac0b
Javier Pérez <javier.perez.ca@usach.cl>
2021-03-09 22:50:13 -0300

    Feat: Added Cequel connection.

--- Web/Back/app/util/cassandra_connector.rb
+++ Web/Back/app/util/cassandra_connector.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
-require 'cassandra'
-
 # Makes a connection to REDIS.
 class CassandraConnector
   @cassandra = nil
   def self.get
-    @cassandra = Cassandra.new('astro_keyspace', '127.0.0.1:9042')
+    @cassandra = Cequel.connect(
+                  :host => '127.0.0.1:9042',
+                  :keyspace => 'astro_keyspace'
+                )
     @cassandra
   end
 end
